db9c035badc88827239cf3d55d7841ea342aba89
Nils Holzer <kroko.holzer@gmail.com>
2024-08-18 23:03:20 +0200

    First average function works

--- p1/pointers.c
+++ p1/pointers.c
@@ -3,8 +3,26 @@
 int average(int *arrayPointer, unsigned int size)
 {
     (void) arrayPointer;
-    (void) size;
-    return 0;
+    int newSize;
+
+    if (size > INT16_MAX) {
+        newSize = INT16_MAX;
+    } else {
+        newSize = (int)size;
+    }
+
+    int sum = 0;
+
+    if(newSize == 0) {
+        return 0;
+    }
+
+    for (int arrayIndex = 0; arrayIndex < newSize; arrayIndex++)
+    {
+        sum = sum + *(arrayPointer + arrayIndex);
+    }
+    
+    return sum / newSize;
 }
 
 int averageIndirect(int **arrayPointer, unsigned int size)
