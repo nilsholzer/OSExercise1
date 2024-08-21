8fef7d08f7889c6e5c1348615abaebb1608a7b74
Nils Holzer <kroko.holzer@gmail.com>
2024-08-21 17:47:42 +0200

    Added .gitignore file, wrote the function and passed all testcases

--- p2/print.c
+++ p2/print.c
@@ -1,8 +1,10 @@
 #include "print.h"
+#include <stdio.h>
+#include <inttypes.h>
 
 void print_line(int64_t number, char *string)
 {
     (void) number;
     (void) string;
-    // Add code here.
+    printf("%" PRId64 " %s\n", number, string);
 }
