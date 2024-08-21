1c7975681c780a5496e588d4eadb9485f0d8d1da
Nils Holzer <kroko.holzer@gmail.com>
2024-08-21 21:35:05 +0200

    Function parseInt is implemented as well as a new function called power. All test cases were passed and I tried to check as much edge cases as possible

--- p3/parseint.c
+++ p3/parseint.c
@@ -14,6 +14,22 @@ int parseDecimalChar(char c)
     return -1;
 }
 
+/**
+ * Calculates the result of base^exponent with recursion.
+ * This function makes the function parseInt easier and smaller.
+ */
+int power(int base, int exponent) {
+    if (exponent == 0) {
+        return 1;
+    }
+    int temp = power(base, exponent/2);
+    if (exponent % 2 == 0){
+        return temp * temp;
+    } else {
+        return base * temp * temp;
+    }
+}
+
 /*
  * Parses a non-negative integer, interpreted as octal when starting with 0,
  * decimal otherwise. Returns -1 on error.
@@ -21,6 +37,42 @@ int parseDecimalChar(char c)
 int parseInt(char *string)
 {
     (void)string;
+    int length = 0;
+    int base;
+    int result = 0;
 
-    return -1;
+    while (string[length] != '\0') {
+        length++;
+    }
+
+    if(length == 0) {
+        return -1;
+    }
+
+    int firstDigit = parseDecimalChar(string[0]);
+
+    if (firstDigit == 0) {
+        base = 8;
+    } else if (firstDigit != -1) {
+        base = 10;
+    } else {
+        return -1;
+    }
+
+    for (int stringIndex = 0; stringIndex < length; stringIndex++)
+    {
+        int digit = parseDecimalChar(string[stringIndex]);
+
+        if (digit == -1){
+            return -1;
+        } else if (digit == 0)
+        {
+            continue;
+        } else {
+            result += digit * power(base, length - 1 - stringIndex);
+        }
+        
+    }
+
+    return result;
 }
