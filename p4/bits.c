4e4c85c281c4d7988ff4cb4922b864c1b5260003
Nils Holzer <kroko.holzer@gmail.com>
2024-09-07 00:16:28 +0200

    getN() function added

--- p4/bits.c
+++ p4/bits.c
@@ -9,7 +9,16 @@ int getN(uint64_t *A, size_t n)
 {
     (void) A;
     (void) n;
-    return 42;
+    
+    uint64_t elementIndex = n / 64;
+    uint64_t elementBit = n % 64;
+    uint64_t element = *(A + elementIndex);
+
+    uint64_t mask = 1 << elementBit;
+    uint64_t masked_n = element & mask;
+    uint64_t bit = masked_n >> elementBit;
+    
+    return bit;
 }
 
 /*
